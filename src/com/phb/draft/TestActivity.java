3ef44d32fd0b9d5caa52d922b53520f93fac9683
Administrator <Administrator@192.168.0.16>
2015-01-23 17:15:51 +0800

    aaaaaaaaaaaaaaaaaaaa

--- src/com/phb/draft/TestActivity.java
+++ src/com/phb/draft/TestActivity.java
@@ -2,6 +2,7 @@ package com.phb.draft;
 
 import com.phb.draft.data.PersonalInfo;
 import com.phb.draft.data.TableContract;
+import com.phb.draft.view.AutoLineLayout;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -18,6 +19,7 @@ public class TestActivity extends Activity implements OnClickListener{
     private Button mBtInsert;
     private Button mBtSearch;
     private TableContract contract;
+    private AutoLineLayout mAll;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,6 +36,7 @@ public class TestActivity extends Activity implements OnClickListener{
 		mEtSearchName = (EditText) findViewById(R.id.et_search_name);
 		mBtInsert = (Button) findViewById(R.id.bt_insert);
 		mBtSearch = (Button) findViewById(R.id.bt_search);
+		mAll = (AutoLineLayout) findViewById(R.id.all);
 	}
 	
 	private void setListeners() {
@@ -49,13 +52,15 @@ public class TestActivity extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.bt_search:
-			PersonalInfo info = contract.query(mEtSearchName.getText().toString());
-			mEtName.setText(info.getName());
-			mEtSex.setText(info.getSex());
-			mEtAge.setText(info.getAge() + "");
+//			PersonalInfo info = contract.query(mEtSearchName.getText().toString());
+//			mEtName.setText(info.getName());
+//			mEtSex.setText(info.getSex());
+//			mEtAge.setText(info.getAge() + "");
+			mAll.addTags(mEtSearchName.getText().toString());
 			break;
 		case R.id.bt_insert:
-			contract.insert(mEtName.getText().toString(), mEtSex.getText().toString(), Integer.valueOf(mEtAge.getText().toString()));
+			//contract.insert(mEtName.getText().toString(), mEtSex.getText().toString(), Integer.valueOf(mEtAge.getText().toString()));
+			mAll.addTags(mEtSearchName.getText().toString());
 			break;
 		default:
 			break;
